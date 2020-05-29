aa136f4d66107af3afcb7d23f50dde09a7c605e1
Loris <rever22411@gmail.com>
2020-05-29 14:11:47 +0200

    basic configuration

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,6 +1,48 @@
 import React from 'react';
 import { Button } from 'antd';
+import { Collapse } from 'antd';
+import { List, Card } from 'antd';
 
-const App = () => <Button type='primary'>Button</Button>;
+import trainingsData from '../assets/config.json';
 
-export default App;
+const { Panel } = Collapse;
+
+class MainPage extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <>
+        <Collapse accordion>
+          {trainingsData.map((section, key) => {
+            return (
+              <Panel header={section.title} key={key}>
+                <List
+                  grid={{
+                    gutter: 16,
+                    xs: 1,
+                    sm: 2,
+                    md: 4,
+                    lg: 4,
+                    xl: 6,
+                    xxl: 3,
+                  }}
+                  dataSource={section.children}
+                  renderItem={(item) => (
+                    <List.Item>
+                      <Card title={item.title}>Card content</Card>
+                    </List.Item>
+                  )}
+                />
+              </Panel>
+            );
+          })}
+        </Collapse>
+      </>
+    );
+  }
+}
+
+export default MainPage;
